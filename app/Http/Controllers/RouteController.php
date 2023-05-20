84da3f7d88982ed97d73edfe00344dfb3e018c0f
ditoputra45 <ditoputra0838@gmail.com>
2023-05-20 22:36:09 +0700

    first commit

---
+++ app/Http/Controllers/RouteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RouteController extends Controller
+{
+    public function index() {
+        return "This is from Controller";
+       }
+
+    public function profile() {
+        return "This is Profile from Controller";
+       }
+}
+
